592600fcd7da18b3a4eb0d939d32fdd9e9cecdc8
SongJi <719792323@qq.com>
2023-10-21 21:58:21 +0800

    2023_10_21

--- start/src/main/java/com/actionworks/flashsale/FlashSaleApplication.java
+++ start/src/main/java/com/actionworks/flashsale/FlashSaleApplication.java
@@ -1,11 +1,22 @@
 package com.actionworks.flashsale;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.jdbc.DataSourceHealthContributorAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.jdbc.DataSourceHealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.metadata.DataSourcePoolMetadataProvider;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.util.StringUtils;
+
+import javax.sql.DataSource;
+import java.util.Map;
 
 @SpringBootApplication(scanBasePackages = {"com.actionworks.flashsale", "com.alibaba.cola"})
 public class FlashSaleApplication {
@@ -18,5 +29,26 @@ public class FlashSaleApplication {
     MeterRegistryCustomizer<MeterRegistry> configurer(
             @Value("${spring.application.name}") String applicationName) {
         return (registry) -> registry.config().commonTags("application", applicationName);
+    }
+}
+
+@Configuration
+class DataSourceHealthConfig extends DataSourceHealthContributorAutoConfiguration {
+
+    @Value("${spring.datasource.dbcp2.validation-query:select 1}")
+    private String defaultQuery;
+
+
+    public DataSourceHealthConfig(Map<String, DataSource> dataSources, ObjectProvider<DataSourcePoolMetadataProvider> metadataProviders) {
+        super(dataSources, metadataProviders);
+    }
+
+    @Override
+    protected AbstractHealthIndicator createIndicator(DataSource source) {
+        DataSourceHealthIndicator indicator = (DataSourceHealthIndicator) super.createIndicator(source);
+        if (!StringUtils.hasText(indicator.getQuery())) {
+            indicator.setQuery(defaultQuery);
+        }
+        return indicator;
     }
 }
